578081812439ff44dc5fa00299c8f69a8f3cba27
ScottieCornett <scottie.cornett@gmail.com>
2023-09-07 18:59:12 -0400

    program added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    Console.Write("Type your name: ");
+    var name = Console.ReadLine();
+    var array = new char[name.Length];
+
+    for (int i = array.Length; i > 0; i--) {
+      array[array.Length - i] = name[i - 1];
+      }
+    var reversed = new string(array);
+    Console.WriteLine("Your name spelled backwards is: " + reversed.ToLower());
+    }
+      
+  }
+
